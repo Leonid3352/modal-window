6f0ae0f686ecafdc6d491fe2acee9a1b0d20e96c
Leonid3352 <157629786+Leonid3352@users.noreply.github.com>
2024-07-12 22:37:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ var btn = document.getElementById('myBtn');
 var span = document.getElementById('cross');
 
 btn.onclick = function () {
+    modal.style.animation = "slideIn 0.5s forwards";
     modal.style.display = "block";
 }
 
 span.onclick = function () {
-    modal.style.display = "none"
+    modal.style.animation = "slideOut 0.5s forwards";
 }
-
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none"
+        modal.style.animation = "slideOut 0.5s forwards";
     }
 }
